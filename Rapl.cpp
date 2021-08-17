7289d4034028e1ef583bc7d55a93395fd6361850
G <guillermotrabes@gmail.com>
2021-08-17 11:23:52 -0400

    Fixes

--- Rapl.cpp
+++ Rapl.cpp
@@ -352,7 +352,7 @@ Rapl::Rapl(int core) {
 
 void Rapl::reset() {
 
-	for(int j=0;j<total_packages;j++){
+	for(j=0;j<total_packages;j++){
 		prev_state[j] = &state1[j];
 		current_state[j] = &state2[j];
 		next_state[j] = &state3[j];
@@ -365,6 +365,12 @@ void Rapl::reset() {
 		running_total[j].psys = 0;
 
 		gettimeofday(&(running_total[j].tsc), NULL);
+
+		prev_state[j]->pkg = 0;
+		prev_state[j]->pp0 = 0;
+		prev_state[j]->pp1 = 0;
+		prev_state[j]->dram = 0;
+		prev_state[j]->psys = 0;
 
 		current_state[j]->pkg = 0;
 		current_state[j]->pp0 = 0;
@@ -468,16 +474,13 @@ void Rapl::measure() {
 
 		close(fd);
 
+		gettimeofday(&(next_state[j]->tsc), NULL);
+
 		next_state[j]->pkg = package_before[j];
 		next_state[j]->pp0 = pp0_before[j];
 		next_state[j]->pp1 = pp1_before[j];
 		next_state[j]->dram = dram_before[j];
 		next_state[j]->psys = psys_before[j];
-
-		cout << "pkg" << next_state[j]->pkg << endl;
-		cout << "pp0" << next_state[j]->pp0 << endl;
-		cout << "pp1" << next_state[j]->pp1 << endl;
-		cout << "dram" << next_state[j]->dram << endl;
 
 		// Update running total
 		running_total[j].pkg += energy_delta(current_state[j]->pkg, next_state[j]->pkg);
@@ -492,7 +495,6 @@ void Rapl::measure() {
 		current_state[j] = next_state[j];
 		next_state[j] = pprev_state;
 
-		gettimeofday(&(next_state[j]->tsc), NULL);
 	}
 }
 
@@ -503,7 +505,8 @@ double Rapl::time_delta(struct timeval *begin, struct timeval *end) {
 
 double Rapl::power(uint64_t before, uint64_t after, double time_delta, int package) {
 	if (time_delta == 0.0f || time_delta == -0.0f) { return 0.0; }
-	double energy = cpu_energy_units[package] * ((double) energy_delta(before,after));
+	//double energy = cpu_energy_units[package] * ((double) energy_delta(before,after));
+	double energy = ((double) energy_delta(before,after));
 	return energy / time_delta;
 }
 
@@ -556,7 +559,7 @@ double Rapl::pp1_average_power(int package) {
 }
 
 double Rapl::dram_average_power(int package) {
-	return dram_total_energy(package) / total_time();
+	return dram_total_energy(package)/total_time();
 }
 
 double Rapl::pkg_total_energy(int package) {
